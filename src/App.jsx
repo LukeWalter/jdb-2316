1ce63f95709fee4f1d580c6d5708ffb074156bfd
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2022-11-21 17:18:13 -0500

    Init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { 
+    Typography
+} from '@mui/material';
+
+const App = () => {
+
+    return (
+
+        <>
+            <Typography>Hello, world!</Typography>
+        </>
+
+    );
+
+} // App
+
+export default App;
